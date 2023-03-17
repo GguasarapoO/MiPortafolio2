a953ff760dd506b74b535e2ca903bb986cfa8b5f
GgusarapoO <losergusarapo@gmail.com>
2023-03-17 15:59:59 -0400

    Add Darkmode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React, { useState } from "react"
 import { Link } from "react-scroll/modules"
 import { useTheme } from "next-themes"
@@ -38,10 +37,25 @@ const Navbar = () => {
                     <h2 className="text-2xl font-bold">Henry Monroy</h2>
                 </div>
             </div>
-            <div className="md:flex md:space-x-6">
+            <div className="md:flex md:space-x-6 items-center justify-center">
                 {NAV_ITEMS.map((item, idx) => {
                     return <a key={idx}>{item.label}</a>
                 })}
+                {currentTheme === "dark" ? (
+                <button 
+                    onClick={() => setTheme("light")}
+                    className="bg-slate-100 p-2 rounded-xl"
+                >
+                <RiSunLine size={25} color="black"/>
+                </button>
+                 ) : (
+                <button
+                    onClick={() => setTheme("dark")}
+                    className="bg-slate-100 p-2 rounded-xl"
+                >
+                <RiMoonFill size={25}/>
+                </button>
+                )}
             </div>
         </div>
     </header>
